af1a7bab7a148c660849519c1c08c878252b4ed4
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2021-09-16 11:25:29 -0300

    utilidades em python

---
+++ python/python.py
@@ -0,0 +1,36 @@
+print("Coisas interessantes em python!")
+
+graph = {1: [2, 3, 4, 5],
+         2: [1, 3, 4, 5]}
+
+print(graph.keys())  # uma lista contendo apenas as chaves
+print(graph.values())  # uma lista contendo apenas os valores.
+print(graph.items())  # tupla (chave, valor).
+
+print(set(graph.keys()))  # um conjunto das chaves do meu dicionário, se fosse valores, iria dar unhashble type error.
+print(len(set(graph.keys())))
+
+conjunto1 = {1, 2, 3, 4, 5, 6, 7, 10, 20, 11}
+conjunto2 = {1, 34, 54, 55, 67, 2, 3, 4, 56, 667}
+
+u = conjunto1 | conjunto2
+u2 = conjunto1.union(conjunto2)
+i = conjunto1 & conjunto2
+i2 = conjunto1.intersection(conjunto2)
+
+print(u)
+print(i)
+
+from collections import Counter
+from collections import namedtuple
+
+lista = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 4, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+print(Counter(lista))
+
+# definição da tupla nomeada.
+user = namedtuple("usuario", 'nome idade sexo nacionalidade')
+
+cadastro = user("Vinicius", 30, "masc", "br")
+
+print(cadastro)
