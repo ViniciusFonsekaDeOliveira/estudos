341a645b18efbef26611390fb795dee69eadaddb
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2022-01-10 17:24:35 -0300

    Utilizando lambda expressions para ordenar o iterável.

---
+++ python-curso/usando_groupby.py
@@ -0,0 +1,21 @@
+from itertools import groupby
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'B', 'matricula': '19.2.4033'},
+    {'nome': 'Pedro', 'nota': 'C', 'matricula': '16.1.1099'},
+    {'nome': 'Gustavo', 'nota': 'A', 'matricula': '18.2.4030'},
+    {'nome': 'Lucas', 'nota': 'A', 'matricula': '20.2.8000'},
+    {'nome': 'Carlos', 'nota': 'C', 'matricula': '19.1.4075'},
+    {'nome': 'Joana', 'nota': 'A', 'matricula': '18.1.6060'},
+    {'nome': 'Bianca', 'nota': 'B', 'matricula': '19.2.4031'},
+    {'nome': 'Alice', 'nota': 'B', 'matricula': '19.1.3920'},
+    {'nome': 'Luana', 'nota': 'A', 'matricula': '17.2.6000'},
+    {'nome': 'Larissa', 'nota': 'B', 'matricula': '20.1.4503'}
+]
+
+ranking_alunos = sorted(alunos, key=lambda item: item['nota'])
+
+alunos_mais_antigos = sorted(alunos, key=lambda item: item['matricula'])
+
+print(f'Melhores alunos: {ranking_alunos}')
+print(f'Alunos mais antigos: {alunos_mais_antigos}')
