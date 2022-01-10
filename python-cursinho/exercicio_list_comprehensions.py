b2dc021d7b974d8507e83674dbf138bce1b3783d
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2022-01-10 16:12:31 -0300

    List comprhensions

---
+++ python-cursinho/exercicio_list_comprehensions.py
@@ -0,0 +1,11 @@
+carrinho = [("Produto 1", 20),
+            ("Produto 2", 20),
+            ("Produto 3", 20),
+            ("Produto 4", 20),
+            ("Produto 5", 20),
+            ("Produto 6", 20),
+            ("Produto 7", 20),
+            ("Produto 8", 20)]
+
+print(carrinho)
+print(sum([preco for _, preco in carrinho]))
