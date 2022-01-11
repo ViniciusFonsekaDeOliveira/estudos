2a7e13832357c2a78898a6e8270ae058f4698a55
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2022-01-11 17:20:40 -0300

    Utilizando if nas list-comprhensions

--- python-curso/exercicio_list_comprehensions.py
+++ python-curso/exercicio_list_comprehensions.py
@@ -3,9 +3,9 @@ carrinho = [("Produto 1", 20),
             ("Produto 3", 20),
             ("Produto 4", 20),
             ("Produto 5", 20),
-            ("Produto 6", 20),
-            ("Produto 7", 20),
+            ("Produto 6", 10),
+            ("Produto 7", 10),
             ("Produto 8", 20)]
 
 print(carrinho)
-print(sum([preco for _, preco in carrinho]))
+print(sum([preco for _, preco in carrinho if preco < 20]))
